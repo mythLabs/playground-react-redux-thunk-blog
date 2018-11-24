c6d8c41f6ec6e4cfa9296b5e226bf87561d9e438
mythLabs <amitbisht744@gmail.com>
2018-11-24 19:50:59 +0530

    Added new action,reducer to get post author

---
+++ src/components/UserHeader.js
@@ -0,0 +1,27 @@
+import React from "react";
+import { connect } from "react-redux";
+
+import { fetchUser } from "../actions";
+
+class UserHeader extends React.Component {
+  componentDidMount() {
+    this.props.fetchUser(this.props.userId);
+  }
+
+  render() {
+    if (!this.props.user) {
+      return null;
+    }
+
+    return <div className="header">{this.props.user.name}</div>;
+  }
+}
+
+const mapStateToProps = (state, ownProps) => {
+  return { user: state.users.find(user => user.id === ownProps.userId) };
+};
+
+export default connect(
+  mapStateToProps,
+  { fetchUser }
+)(UserHeader);
